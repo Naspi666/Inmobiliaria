178314d6cc7deaeb75933a4bf184de2cea8e651b
Sebastian Leal S <sedastianleal@gmai.com>
2021-11-20 15:50:18 -0500

    commit menor

--- src/server.js
+++ src/server.js
@@ -31,6 +31,10 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// errorhandlers
+
+
+// messages
 app.use((req, res, next) => {
   app.locals.signupMessage = req.flash('signupMessage');
   app.locals.signinMessage = req.flash('signinMessage');
